ba49e4155fde2dcd29b12fb30ca883bb38bab5a3
qwizor1337 <gasuz0r@gmail.com>
2021-09-27 12:57:28 +0300

    HW9

--- Ozon/lesson_9/wiki_search.py
+++ Ozon/lesson_9/wiki_search.py
@@ -1,6 +1,7 @@
 #Задание №4
 import requests
 from bs4 import BeautifulSoup
+import re
 # Ссылка на наш источник парсинга
 
 url = 'https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%B1%D1%8C%D1%8E%D0%BB%D0%B0'
@@ -10,6 +11,8 @@ resp = requests.get(url).text
 
 # Создание объекта для париснга html
 soup = BeautifulSoup(resp, 'html.parser')
-
+str_to_detect = 'wikipedia'
 for el in soup.find_all('a'):
-    print(el.get('href'))
+    if (re.search(str_to_detect, str(el.get('href'))) == None) == True:
+        if len(re.findall('https?', str(el))) > 0:
+            print(el.get('href'))
